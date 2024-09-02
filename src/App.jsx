af1c07ad491899a320e9386dbb043b453a78c432
kashfasehejatsezuti <you@example.com>
2024-09-02 02:28:10 +0200

    Updated New Features

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+//
 import "./App.css";
 import { useState } from "react";
+import Logo from "../public/vite.png";
 
 function App() {
   const [jsonData, setJsonData] = useState(null);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [isDisabledDownload, setIsDisabledDownload] = useState(false);
 
   const convertCsvToJson = (csvData) => {
     const lines = csvData.split("\n");
@@ -13,7 +17,7 @@ function App() {
     for (let i = 1; i < lines.length; i++) {
       const obj = {};
       const currentLine = lines[i].split(",");
-      //Loop  go each column of csv data and map it corresponding heading.
+      // Loop  go each column of csv data and map it corresponding heading.
       for (let j = 0; j < headers.length; j++) {
         obj[headers[j].trim()] = currentLine[j] ? currentLine[j].trim() : "";
       }
@@ -36,7 +40,9 @@ function App() {
     render.readAsText(file);
   };
 
-  const handleCopy = () => {
+  const handleCopy = (e) => {
+    e.target.innerText = "Done";
+    setIsDisabled(true);
     const jsonString = JSON.stringify(jsonData, null, 2);
     navigator.clipboard
       .writeText(jsonString)
@@ -48,14 +54,16 @@ function App() {
       });
   };
 
-  const downloadJsonFile = () => {
+  const downloadJsonFile = (e) => {
+    e.target.innerText = "Done";
+    setIsDisabledDownload(true);
     const jsonString = JSON.stringify(jsonData, null, 2);
     const jsonFile = new Blob([jsonString], { type: "application/json" });
     const fileUrl = URL.createObjectURL(jsonFile);
     const link = document.createElement("a");
     link.href = fileUrl;
     console.log(fileUrl);
-    link.download = "data.json";
+    link.download = "Converted_File.json";
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -72,7 +80,7 @@ function App() {
             {headers.map((header) => (
               <th
                 key={header}
-                style={{ border: "3px solid green", padding: "8px" }}
+                style={{ border: "1.5px solid ", padding: "8px" }}
               >
                 {header}
               </th>
@@ -85,7 +93,7 @@ function App() {
               {headers.map((header) => (
                 <td
                   key={header}
-                  style={{ border: "2px solid green", padding: "8px" }}
+                  style={{ border: "1px solid ", padding: "8px" }}
                 >
                   {row[header]}
                 </td>
@@ -96,71 +104,123 @@ function App() {
       </table>
     );
   };
+
   return (
     <>
-      <div>
-        <input type="file" accept=".csv" onChange={handelCSVInputChange} />
+      <div style={{ textAlign: "center", margin: "2rem", width: "100%" }}>
+        <img
+          src={Logo}
+          alt="Description"
+          style={{ width: "25%", maxWidth: "500px", margin: "auto" }}
+        />
+        <label
+          htmlFor="file-upload"
+          style={{
+            fontSize: "1.5rem",
+            color: "#2f5f98",
+            backgroundColor: "white",
+            borderRadius: "8px",
+            cursor: "pointer",
+            display: "block",
+          }}
+        >
+          <h1>CSV TO JSON</h1>
+          To get started, upload or paste your data from Excel (saved as CSV ).
+        </label>
+        <input
+          style={{
+            fontSize: "1rem",
+            color: "#2f5f98",
+            padding: "1rem 1rem",
+            backgroundColor: "white",
+            border: "4px solid #2f5f98",
+            borderRadius: "12px",
+            cursor: "pointer",
+            display: "inline-block",
+            margin: "2rem",
+          }}
+          id="file-upload"
+          type="file"
+          accept=".csv"
+          onChange={handelCSVInputChange}
+        />
         {jsonData ? (
           <>
-            <div>
-              <button
-                style={{
-                  fontSize: "1.5rem",
-                  textAlign: "center",
-                  color: "#4ea28e",
-                  margin: "5rem",
-                  justifyItems: "center",
-                  backgroundColor: "white",
-                }}
-                onClick={handleCopy}
-              >
-                Copy
-              </button>
-            </div>
+            {/* Container for the parallel display */}
             <div
               style={{
-                fontSize: "1.5rem",
-                textAlign: "center",
                 display: "flex",
-                width: "auto",
-                height: "35rem",
-                justifyContent: "center",
-                overflowX: "hidden",
-                overflowY: "auto",
-                backgroundColor: "#4ea28e",
-                color: "white",
+                justifyContent: "space-around",
+                alignItems: "flex-start",
+                margin: "1rem",
               }}
             >
-              {<pre>{JSON.stringify(jsonData, null, 2)}</pre>}
-            </div>
-            <div
-              style={{
-                color: "#4ea28e",
-                backgroundColor: "white",
-                marginRight: "4rem",
-                marginTop: "4rem",
-              }}
-            >
-              {renderTable(jsonData)}
+              <div
+                style={{
+                  flex: "1",
+                  marginRight: "2rem",
+                  overflowY: "auto",
+                  height: "31.4rem",
+                  backgroundColor: "#2f5f98",
+                  color: "white",
+                  borderRadius: "3px",
+                  fontSize: "1rem",
+                }}
+              >
+                <pre style={{ whiteSpace: "pre-wrap" }}>
+                  {JSON.stringify(jsonData, null, 3)}
+                </pre>
+              </div>
+
+              <div
+                style={{
+                  flex: "1",
+                  backgroundColor: "white",
+                  color: "#2f5f98",
+                  overflowY: "auto",
+                  height: "31.4rem",
+                  borderRadius: "3px",
+                  fontSize: "1rem",
+                }}
+              >
+                {renderTable(jsonData)}
+              </div>
             </div>
 
             <button
               style={{
-                fontSize: "1.5rem",
+                fontSize: "1rem",
                 textAlign: "center",
-                color: "#4ea28e",
+                color: "#2f5f98",
                 margin: "5rem",
-                justifyItems: "center",
+                justifyItems: "start",
                 backgroundColor: "white",
+                border: "2px solid #2f5f98",
               }}
               onClick={downloadJsonFile}
+              disabled={isDisabledDownload}
             >
-              Download
+              Download The File
+            </button>
+            <button
+              style={{
+                fontSize: "1rem",
+                textAlign: "center",
+                color: "#2f5f98",
+                margin: "5rem",
+                justifyItems: "end",
+                backgroundColor: "white",
+                border: "2px solid #2f5f98",
+              }}
+              onClick={handleCopy}
+              disabled={isDisabled}
+            >
+              Copy The Texts
             </button>
           </>
         ) : (
-          <div style={{ fontSize: "1.5rem" }}>
-            <br></br> Please choose a csv file.
+          <div style={{ fontSize: "1.2rem", color: "#2f5f98" }}>
+            Upload a CSV file
           </div>
         )}
       </div>
